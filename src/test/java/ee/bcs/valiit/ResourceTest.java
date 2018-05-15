313c380b66f9dd706933e2fadd26d0aa4af96f04
Anton Sokolov <sokolovmeister@gmail.com>
2018-05-15 15:20:03 +0300

    Unnecessary Test removed

--- src/test/java/ee/bcs/valiit/ResourceTest.java
+++ src/test/java/ee/bcs/valiit/ResourceTest.java
@@ -2,13 +2,13 @@ package ee.bcs.valiit;
 
 import java.sql.SQLException;
 
-import org.junit.Test;
+/*import org.junit.Test;*/
 
 public class ResourceTest {
-
+/*
 	@Test
 	public void testRes() throws ClassNotFoundException, SQLException {
 //		System.out.println(new ee.bcs.valiit.MyResource().getObject2());
-	}
+	}*/
 
 }
